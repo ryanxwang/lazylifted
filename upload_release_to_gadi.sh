9b62dee5baedaa22d119b0cec0b247bdc602b8e3
Ryan Wang <ryanxiaowang2001@gmail.com>
2024-04-07 21:41:24 +1000

    Lower python dependencies version to be compatible with gadi

---
+++ upload_release_to_gadi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <tag>"
+    exit 1
+fi
+
+tag=$1
+echo "Downloading release $tag to /tmp/$tag"
+gh release download $tag -p "*" --clobber -D /tmp/$tag
+
+gadi_dir="/home/444/rw8037/lazylifted"
+echo "Uploading release $tag to Gadi at $gadi_dir"
+ssh rw8037@gadi.nci.org.au "mkdir -p $gadi_dir"
+scp -r /tmp/$tag rw8037@gadi.nci.org.au:$gadi_dir
+ssh rw8037@gadi.nci.org.au "chmod +x $gadi_dir/$tag/*"
