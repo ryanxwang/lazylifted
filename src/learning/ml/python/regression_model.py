a8edaf731d4d1047e7b2146e4525f54b5bfc46ac
Ryan Wang <ryanxiaowang2001@gmail.com>
2024-05-05 18:53:56 +1000

    Fix linear regression

--- src/learning/ml/python/regression_model.py
+++ src/learning/ml/python/regression_model.py
@@ -17,12 +17,14 @@ class RegressionModel:
     def fit(self, X, y):
         self.model.fit(X, y)
         self.weights = self.get_weights()
+        self.bias = self.get_bias()
 
     def predict(self, X):
         # for efficiency, we don't use the predict method of the model but
         # compute the prediction directly
         assert hasattr(self, "weights") and self.weights is not None
-        return X @ self.weights.T
+        assert hasattr(self, "bias") and self.bias is not None
+        return X @ self.weights.T + self.bias
 
     def get_weights(self):
         if self.model_str == "lr":
@@ -31,13 +33,21 @@ class RegressionModel:
             # this only works for GPR with DotProduct kernel
             return self.model.alpha_ @ self.model.X_train_
 
+    def get_bias(self):
+        if self.model_str == "lr":
+            return self.model.intercept_
+        elif self.model_str == "gpr":
+            return 0
+
     def __getstate__(self):
         return {
             "model_str": self.model_str,
             "weights": self.weights,
+            "bias": self.bias,
         }
 
     def __setstate__(self, state):
         self.model_str = state["model_str"]
         self.model = None
         self.weights = state["weights"]
+        self.bias = state["bias"]
