f5399d889dde6a28fe43ab4b300784a7b361159f
Ryan Wang <ryanxiaowang2001@gmail.com>
2024-09-06 12:28:28 +1000

    Update some todos

--- src/learning/models/wl_model_config.rs
+++ src/learning/models/wl_model_config.rs
@@ -4,6 +4,9 @@ use crate::learning::{
 };
 use serde::{Deserialize, Serialize};
 
+// TODO-someday: the resulting config files are ugly and hard to read. Part of
+// this is that toml doesn't seem enums nicely, but I've also just not put much
+// effort into making the config files look nice.
 #[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct WlModelConfig {
@@ -18,8 +21,10 @@ pub struct WlModelConfig {
     /// true.
     #[serde(default = "default_tune")]
     pub tune: bool,
-    // TODO-soon: this really should be a command line flag, not a part of the
+    // TODO-someday: this really should be a command line flag, not a part of the
     // model config, but this is convenient for now
+    //
+    // UPDATE: This might actually be fine/good? Changing to a "someday".
     #[serde(default = "default_explain_colours")]
     pub explain_colours: bool,
     /// Whether to round heuristic values to the nearest integer.
