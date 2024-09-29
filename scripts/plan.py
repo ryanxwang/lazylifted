7aba7855db560540efe51f30b62c0c6e30cc3ff8
Ryan Wang <ryanxiaowang2001@gmail.com>
2024-09-29 21:16:26 +1000

    Fix plan script for blocksworld-hbf

--- scripts/plan.py
+++ scripts/plan.py
@@ -33,7 +33,7 @@ def main():
     instance = args.instance
     domain = args.domain
     if args.domain.endswith("-hbf"):
-        domain_dir = f"benchmarks/hbf/{domain}"
+        domain_dir = f"benchmarks/hbf/{domain[:-4]}"
     else:
         domain_dir = f"benchmarks/ipc23-learning/{domain}"
 
